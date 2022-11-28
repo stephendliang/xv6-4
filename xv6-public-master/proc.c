c6a2cbc007ece91b577393db3111ddc5b497bcd5
Stephen Liang <sl@sent.at>
2022-11-28 01:00:47 -0600

    vm

--- xv6-public-master/proc.c
+++ xv6-public-master/proc.c
@@ -177,6 +177,48 @@ growproc(int n)
 // Create a new process copying p as the parent.
 // Sets up stack to return as if from system call.
 // Caller must set state of returned proc to RUNNABLE.
+
+/**
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  // Copy process state from proc.
+  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+
+  np->state = RUNNABLE;
+
+  release(&ptable.lock);
+
+  return pid;*/
+  
 int
 fork(void)
 {
--- xv6-public-master/vm.c
+++ xv6-public-master/vm.c
@@ -312,6 +312,7 @@ clearpteu(pde_t *pgdir, char *uva)
 
 // Given a parent process's page table, create a copy
 // of it for a child.
+/*
 pde_t*
 copyuvm(pde_t *pgdir, uint sz)
 {
@@ -342,7 +343,53 @@ copyuvm(pde_t *pgdir, uint sz)
 bad:
   freevm(d);
   return 0;
+}*/
+
+
+pde_t*
+copyuvm(pde_t *pgdir, uint sz)
+{
+  pde_t *d;
+  pte_t *pte;
+  uint pa, i, flags;
+  //char *mem;
+  
+  if((d = setupkvm()) == 0)
+    return 0;
+
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+
+    *pte = (~ PTE_W) & *pte;  // read only parent page
+
+    flags = PTE_FLAGS(*pte);
+
+    pa = PTE_ADDR(*pte);
+
+    /*flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    */
+
+    if(mappages(d, (void*)i, PGSIZE, pa, flags) < 0) {
+      //kfree(mem);
+        freevm(d);
+        lcr3(V2P(pgdir));   // flush tlb
+        
+        return 0;
+    }
+    increment_refC(pa);
+  }
+
+  lcr3(V2P(pgdir));
+  return d;
 }
+
+
 
 //PAGEBREAK!
 // Map user virtual address to kernel address.
